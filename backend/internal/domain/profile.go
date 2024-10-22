e5450222d4a85c486403c799e6641e2356a317d5
nikola.sovilj <nikola.sovilj@crqlar.com>
2024-10-22 12:00:50 +0200

    Created sidebar & updated navigation

--- backend/internal/domain/profile.go
+++ backend/internal/domain/profile.go
@@ -3,6 +3,10 @@ package domain
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 // Profile represents a user profile in the system
+// This needs to be changed to generic entity
+// 1) Fetch the data
+// 2) Create the merge candidates with similarity scores in database
+// 3) Create and store merge suggestions with final similarity score
 type Profile struct {
 	ID          primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
 	FirstName   string             `json:"first_name" bson:"first_name"`
